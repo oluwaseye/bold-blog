7656bc3acc1c755da49a64468b6384fb6e7dfc50
Seye <seyz4all@gmail.com>
2019-08-27 17:38:58 -0400

    New updates to style and home

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
       center: true,
       padding: '2rem'
     },
+    fontFamily: {
+      'serif': ['Lato', 'Helvetica', 'sans-serif'],
+      'sans': ['Roboto', 'Arial', 'sans-serif'],
+    },
+    height: {
+      '70': '20rem'
+    }
   },
   variants: {},
   plugins: []
