e2e2d2385c146f63d38b9894bee3dedcf29d2018
Seye <seyz4all@gmail.com>
2019-08-30 09:56:44 -0400

    Title and scroll updates

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,13 @@ $( document ).ready(function() {
         if (scroll > 100) {
             $("#topbar").addClass("bg-black"); 
             $("#topbar-container").removeClass("border-b-2"); 
-            $("#topbar-container").removeClass("py-6"); 
+            $("#topbar-container").removeClass("py-6");
+            $("#topbar-container").addClass("py-3");
         } else {
             $("#topbar").removeClass("bg-black"); 
             $("#topbar-container").addClass("border-b-2");
             $("#topbar-container").addClass("py-6");  
+            $("#topbar-container").removeClass("py-3");
         }
     });
 });
