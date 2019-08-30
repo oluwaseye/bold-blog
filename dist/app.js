e5093bb5d7bd2be8dd0b6828ebde17609b322a08
Oluwaseye <seyz4all@gmail.com>
2019-08-30 08:15:08 -0400

    Added Tailwind transition plugin and fixed some issues

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {  
+   $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+            $("#topbar").addClass("bg-black"); 
+            $("#topbar-container").removeClass("border-b-2"); 
+            $("#topbar-container").removeClass("py-6"); 
+        } else {
+            $("#topbar").removeClass("bg-black"); 
+            $("#topbar-container").addClass("border-b-2");
+            $("#topbar-container").addClass("py-6");  
+        }
+    });
+});
